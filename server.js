6ddb139649b7c2d316e5018f24cd7fe4c11e3303
Jmshealy1 <jmshealy@email.sc.edu>
2025-04-08 09:00:46 -0400

    images

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path")
 const app = express();
 
 app.use(cors());
 app.use(express.static("public"));
-app.use("/images", express.static("images"));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 const gear = [
   {
