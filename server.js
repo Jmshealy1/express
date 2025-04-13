94836c9cd0f11c029761d15eccc481998a533abf
Jmshealy1 <jmshealy@email.sc.edu>
2025-04-13 15:46:59 -0400

    .

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ let gear = [
     pricePerDay: 55,
     rating: 4.8,
     description: "Bolt-action hunting rifle with exceptional accuracy.",
-    main_image: "images/remington700.jpg"
+    main_image: "images/remington700.jpg",
   },
   {
     _id: 2,
@@ -40,64 +40,9 @@ let gear = [
     pricePerDay: 45,
     rating: 4.9,
     description: "Semi-automatic shotgun ideal for waterfowl.",
-    main_image: "images/benelli.jpg"
-  },
-  {
-    _id: 3,
-    name: "CVA Wolf Black Powder Rifle",
-    material: "Wood & Iron",
-    pricePerDay: 40,
-    rating: 4.5,
-    description: "Classic muzzleloader rifle, accurate and reliable.",
-    main_image: "images/cva.jpg"
-  },
-  {
-    _id: 4,
-    name: "Leupold VX-Freedom",
-    material: "Aluminum & Glass",
-    pricePerDay: 20,
-    rating: 4.8,
-    description: "High-quality rifle scope with crystal-clear optics.",
-    main_image: "images/leupold.jpg"
-  },
-  {
-    _id: 5,
-    name: "Vortex Diamondback",
-    material: "Aluminum & Multi-coated Glass",
-    pricePerDay: 18,
-    rating: 4.7,
-    description: "Budget-friendly scope with excellent optics.",
-    main_image: "images/vortex.jpg"
-  },
-  {
-    _id: 6,
-    name: "Irish Setter Hunting Boots",
-    material: "Leather & Gore-Tex",
-    pricePerDay: 10,
-    rating: 4.9,
-    description: "Waterproof boots suitable for rugged terrain.",
-    main_image: "images/irishsetter.jpg"
-  },
-  {
-    _id: 7,
-    name: "Eberlestock Backpack",
-    material: "Ripstop Nylon",
-    pricePerDay: 8,
-    rating: 4.8,
-    description: "Durable backpack for long hunting trips.",
-    main_image: "images/eberlestock.jpg"
-  },
-  {
-    _id: 8,
-    name: "Badlands 2200 Pack",
-    material: "Cordura Nylon",
-    pricePerDay: 12,
-    rating: 4.8,
-    description: "Heavy-duty hunting backpack with ample storage.",
-    main_image: "images/badlands.jpg"
+    main_image: "images/benelli.jpg",
   }
 ];
-
 
 app.get("/api/gear", (req, res) => {
   res.send(gear);
