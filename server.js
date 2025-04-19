8341a1d6c886325a98c12d2a9d3f13861b9b12c2
Jmshealy1 <jmshealy@email.sc.edu>
2025-04-19 19:22:33 -0400

    mongodb

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 const Joi = require("joi");
-
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads")); 
@@ -18,6 +18,29 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage: storage });
+
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://jonathanmshealy:jmshealy123456789@cluster0.c6yfrzv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
+
+const schema = new mongoose.Schema({
+  name: String,
+});
+
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
+
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World",
+});
+
+createMessage();
 
 let gear = [
   {
